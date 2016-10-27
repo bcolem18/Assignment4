16e1c1459e465d5208b76cc8e0ddb46e2836e7c7
bcolem18 <bcolem18@uncc.edu>
2016-10-27 05:06:17 +0000

    Part4

--- Part3.cpp
+++ Part3.cpp
@@ -5,6 +5,9 @@
 #include <algorithm>
 #include <functional>
 #include <iterator>
+#include<chrono>
+#include<ctime>
+#include<time.h>
 using namespace std;
 
 
@@ -21,29 +24,17 @@ double func_1(double x, int intensity)
 
 
 
-double integral (double a, double b, int N, int intensity)
+
+
+
+
+
+
+
+
+int main(int argc, char*argv[])//set number of threads, a, b, intensity, 
 {
-
-	double sum=0;
-
-	 for(int i=0; i<N; i++)//
-			{
-				const double h= (b-a)/N;
-				sum+= func_1(a + (i+.5) *h ,intensity)  * h;
-
-			}
-		return sum;
-	}
-
-
-
-
-
-
-
-int main(int argc, char*argv[])
-{
-    if(argc==4)
+    if(argc==6)
     {
         
     double a; 
@@ -51,16 +42,49 @@ int main(int argc, char*argv[])
 	int N; 
 	int intensity; 
 	
+	
 	a= atof(argv[1]); 
 	b=atof(argv[2]); 
 	N=atoi(argv[3]); 
 	intensity=atoi(argv[4]);
+	 int threadNum= atof(argv[5]);
+	 double sum=0; 
+	 
+	 
+	 omp_set_num_threads(threadNum); 
+omp_set_schedule(omp_sched_dynamic, 100000);
+	
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+
+start = std::chrono::system_clock::now();
 	
 	
+	#pragma omp parallel
+	{
+		
+		
+		#pragma omp for schedule(runtime)
+		
+		for(int i=0; i<N; i++)//
+			{
+				const double h= (b-a)/N;
+				sum+= func_1(a + (i+.5) *h ,intensity)  * h;
+
+			}
+		
+			
+		
+	}
         
         
-        
-        
+ 	cout << sum<< endl;       
+        end = std::chrono::system_clock::now();
+ std::chrono::duration<double> elapsed_seconds = end-start;
+ std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+
+  std::cout << "finished acomputation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";   
         
         
         
