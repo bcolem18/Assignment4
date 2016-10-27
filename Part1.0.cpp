3b5b2c2449bef7dca8b2f62a2aa737e4764bc7a2
bcolem18 <bcolem18@uncc.edu>
2016-10-27 01:11:04 +0000

    Yess!

--- Part1.0.cpp
+++ Part1.0.cpp
@@ -46,7 +46,7 @@ int main(int argc, char *argv[]) //Size of array, number of thread, scheduling p
 
  
 omp_set_num_threads(threadNum); 
-omp_set_schedule(omp_sched_dynamic, 10000);
+omp_set_schedule(omp_sched_dynamic, 100000);
  
 
 std::chrono::time_point<std::chrono::system_clock> start, end;
