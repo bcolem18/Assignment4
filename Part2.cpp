41ae5086f6df7c12c968226fb374dc1d42455b99
bcolem18 <bcolem18@uncc.edu>
2016-10-27 02:18:25 +0000

    Change2

--- Part2.cpp
+++ Part2.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 
  
 omp_set_num_threads(threadNum); 
- omp_set_schedule(omp_sched_dynamic, 1);
+ omp_set_schedule(omp_sched_dynamic, 1000);
  
  
 std::chrono::time_point<std::chrono::system_clock> start, end;
