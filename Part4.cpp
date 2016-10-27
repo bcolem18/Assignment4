a6e99e73901de0c21be3acbfaa8c2d699f63f77e
bcolem18 <bcolem18@uncc.edu>
2016-10-27 02:50:21 +0000

    PART4

--- Part4.cpp
+++ Part4.cpp
@@ -10,24 +10,54 @@ using namespace std;
 
 
 
-int main()
+int main(int argc, char *argv[]) //size of array and num of thread
 {
     
-    int arr[19]= {1,1,1,1,5,5,5,5,5,7,7,7,7,8,9,10,11,12,55};
+    if(argc==4) 
+    
+    {
+    
+    int N= atof(argv[1]);
+    int threadNum= atof(argv[2]); 
+    
+    
+   // int arr[19]= {1,1,1,1,5,5,5,5,5,7,7,7,7,8,9,10,11,12,55};
     //int size= arr.length; 
     
-    int val=7; 
+     
+
+    
+    int val=atof(argv[3]); 
     //int min=0; 
-    int pos=20; 
+    int pos=N+1; 
     int result=0; 
     
+    
+    srand(time(NULL)); 
+ 
+ 
+ int *arr= new int[N]; 
+ 
+  for(int i=0; i<N; i++)
+ {
+     
+     arr[i]= (rand()%100)+1; 
+    
+    
+     
+
+     
+     
+     
+     
+ } //generate array 
     
     #pragma omp parallel 
     {
         
        #pragma omp for reduction(min:pos)
         
-            for(int i=0; i<19; i++)
+            for(int i=0; i<N; i++)
                 {
                    // result+=arr[i]; 
                   
@@ -51,7 +81,7 @@ int main()
         
         
         
-        
+    } 
         
         
     }
